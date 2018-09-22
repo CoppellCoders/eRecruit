626ff8c0efc242b5a8e105b9ee99784837f8b45c
ntindle <nickandmason@hotmail.com>
2018-09-22 14:44:49 -0500

    Added part of getProfileStats

--- commands/pubg.js
+++ commands/pubg.js
@@ -19,6 +19,19 @@ module.exports.run=async(bot, message, args) =>{
 
        return message.channel.send(embed);
    }
+   async function getProfileStats(username) {
+    //Accessses the API 
+    let {body} = await superagent
+        .get(`https://api.pubg.com/shards/pc-na/players?filter[playerNames]=${username}`)
+        .on("error", err=>{
+            return message.channel.send(
+                "Error occured while retrieving player stats. Please try again later.\n\n **If this problem keeps arising, make sure you use the `!issue` command to report any issues with the bot**"
+            );
+        });
+    
+    //APU error Handling
+
+   }
 }
 
 module.exports.help = {
