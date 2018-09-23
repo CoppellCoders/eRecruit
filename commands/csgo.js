a0b9b17c7fb99acd58eac6200a7ee16a4bed3ad9
Anirudh Emmadi <kanna6501@gmail.com>
2018-09-23 13:11:11 -0500

    Added CSGO Stats

--- commands/csgo.js
+++ commands/csgo.js
@@ -7,7 +7,7 @@ module.exports.run = async (bot, message, args) => {
   let username = args[0];
   let steamID = await getSteamID(username);
   username = steamID.id;
-  let playerStats = await getPlayerStats(username);
+  let playerStats = await getPlayerStats(username, args[0]);
 
   async function getPlayerStats(username) {
     var stats = {};
@@ -16,36 +16,37 @@ module.exports.run = async (bot, message, args) => {
       id: username
     }).then(r => {
       let stats = r.body.playerstats.stats;
-
-      let kills = stats.total_kills;
-      let headshots = stats.total_kills_headshot;
-      let deaths = stats.total_deaths;
-      let time = stats.total_time_played;
-      let plants = stats.total_planted_bombs;
-      let defuses = stats.total_defused_bombs;
-      let wins = stats.total_wins;
-      let mvps = stats.total_mvps;
-      let matchesPlayed = stats.total_matches_played;
-      let matchesWon = stats.total_matches_won;
-      let shotsFired = stats.total_shots_fired;
-      let shotsHit = stats.total_shot_hit;
-
+      // console.log(stats);
+      let kills = stats[0].value;
+      let headshots = stats[24].value;
+      let deaths = stats[1].value;
+      let time = stats[2].value;
+      let plants = stats[3].value;
+      let defuses = stats[4].value;
+      let mvps = stats[93].value;
+      let matchesPlayed = stats[113].value;
+      let matchesWon = stats[112].value;
+      let shotsFired = stats[42].value;
+      let shotsHit = stats[41].value;
+      // console.log(kills);
       let embed = new Discord.RichEmbed()
         .setTitle(`## CSGO STATS FOR ${args[0].toUpperCase()} ##`)
         .setThumbnail(steamID.avatar)
+        .setColor("#4298f4")
         .addField("Profile Overview", `Overall Stats for **${args[0]}**`)
-        .addField("Kills", playerStats.profile.kills, true)
-        .addField("Accuracy", playerStats.profile.shotsHit / playerStats.profile.shotsFired, true)
-        .addField("Headshots", playerStats.profile.headshots, true)
-        .addField("HS %", playerStats.profile.headshots / playerStats.profile.shotsFired, true)
-        .addField("Time Played", playerStats.profile.time / 60, true)
-        .addField("Competitive Wins", playerStats.profile.matchesWon, true)
-        .addField("Win %", playerStats.profile.matchesWon / playerStats.profile.matchesPlayed, true)
-        .addField("MVPs", playerStats.profile.mvps, true);
+        .addField("Kills", kills, true)
+        .addField("Accuracy", `${Math.round((shotsHit / shotsFired) * 10000) / 100}%`, true)
+        .addField("Headshots", headshots, true)
+        .addField("HS %", `${Math.round((headshots / kills) * 10000) / 100}%`, true)
+        .addField("Time Played", `${Math.ceil(time / 3600)} hours`, true)
+        .addField("Competitive Wins", matchesWon, true)
+        .addField("Win %", `${Math.round((matchesWon / matchesPlayed) * 10000) / 100}%`, true)
+        .addField("MVPs", mvps, true);
 
-      return message.channel.send(embed);
+      message.channel.send(embed);
     }).catch(e => {
-      return message.channel.send("Unknow error Occurred. Please try again. If this issue keeps arising, use the **`.issue`** command to report it.");
+      console.log(e);
+      message.channel.send("Unknow error Occurred. Please try again. If this issue keeps arising, use the **`.issue`** command to report it.");
     })
   }
 
