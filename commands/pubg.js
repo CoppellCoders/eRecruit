44733b4f1d40f641f676be63a587eae932c24589
Nicholas Tindle <nickandmason@hotmail.com>
2018-09-24 16:24:02 -0500

    fix spelling errors

--- commands/pubg.js
+++ commands/pubg.js
@@ -19,29 +19,29 @@ module.exports.run=async(bot, message, args) =>{
        return message.channel.send(embed);
    }
    async function getProfileStats(username) {
-    //Accessses the API 
+    //Accesses the API 
     let {body} = await superagent
         .get(`https://api.pubg.com/shards/pc-na/players?filter[playerNames]=${username}`)
         .set("Authorization", process.env.pubg_key)
         .set("accept", "application/vnd.api+json")
         .on("error", err=>{
             return message.channel.send(
-                "Error occured while retrieving player stats. Please try again later.\n\n **If this problem keeps arising, make sure you use the `!issue` command to report any issues with the bot**"
+                "Error occurred while retrieving player stats. Please try again later.\n\n **If this problem keeps arising, make sure you use the `!issue` command to report any issues with the bot**"
             );
         });
     console.log(body);
     //API error Handling
         if(Object.keys(body).length===0){
             return message.channel.send(
-                "Error occured while retrieving player stats. Please try again later"
+                "Error occurred while retrieving player stats. Please try again later"
             );
            
         }
         let stats = body.data;
         let id = stats.id;
-        let playername=stats[0].attributes.name;
+        let playerName=stats[0].attributes.name;
         return{
-         name: playername, 
+         name: playerName, 
          id: id   
          };
    }
